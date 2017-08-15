7b60ed6be0b0a03f483fdae51981cb12c7d20b0a
Stefan Scherzinger <scherzin@fzi.de>
2017-08-15 07:57:11 +0200

    Add boilerplate code for a joint to Cartesian converter controller
    This controller can be used to turn joint control commands into
    tf end-effector poses, which are used by the Cartesian controllers.

---
+++ joint_to_cartesian_controller/include/joint_to_cartesian_controller/joint_to_cartesian_controller.h
@@ -0,0 +1,43 @@
+// -- BEGIN LICENSE BLOCK -----------------------------------------------------
+// -- END LICENSE BLOCK -------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+/*!\file    joint_to_cartesian_controller.h
+ *
+ * \author  Stefan Scherzinger <scherzin@fzi.de>
+ * \date    2017/08/15
+ *
+ */
+//-----------------------------------------------------------------------------
+
+#ifndef JOINT_TO_CARTESIAN_CONTROLLER_H_INCLUDED
+#define JOINT_TO_CARTESIAN_CONTROLLER_H_INCLUDED
+
+// ROS
+#include <ros/ros.h>
+
+// ros_controls
+#include <controller_interface/controller.h>
+#include <hardware_interface/joint_command_interface.h>
+
+namespace joint_to_cartesian_controller
+{
+
+class JointToCartesianController
+  : public controller_interface::Controller<hardware_interface::PositionJointInterface>
+{
+  public:
+    JointToCartesianController();
+
+    bool init(hardware_interface::PositionJointInterface* hw, ros::NodeHandle& nh);
+
+    void starting(const ros::Time& time);
+
+    void stopping(const ros::Time& time);
+
+    void update(const ros::Time& time, const ros::Duration& period);
+};
+
+}
+
+#endif
---
+++ joint_to_cartesian_controller/src/joint_to_cartesian_controller.cpp
@@ -0,0 +1,65 @@
+// -- BEGIN LICENSE BLOCK -----------------------------------------------------
+// -- END LICENSE BLOCK -------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+/*!\file    joint_to_cartesian_controller.cpp
+ *
+ * \author  Stefan Scherzinger <scherzin@fzi.de>
+ * \date    2017/08/15
+ *
+ */
+//-----------------------------------------------------------------------------
+
+// Pluginlib
+#include <pluginlib/class_list_macros.h>
+
+// Project
+#include <joint_to_cartesian_controller/joint_to_cartesian_controller.h>
+
+namespace cartesian_controllers
+{
+  /**
+   * @brief Turn joint positions into end-effector pose
+   *
+   * The name JointTrajectoryController is somewhat misleading for this
+   * controller, since it only turns joint positions into end-effector poses
+   * without interpolation. The only reason for this is, that having the
+   * substring JointTrajectoryController in the loaded controller type assures
+   * compatibility with the rqt joint trajectory controller plugin, which
+   * checks for this substring when looking for controllers.
+   */
+  typedef joint_to_cartesian_controller::JointToCartesianController JointTrajectoryController;
+}
+
+PLUGINLIB_EXPORT_CLASS(cartesian_controllers::JointTrajectoryController, controller_interface::ControllerBase)
+
+
+
+
+
+
+namespace joint_to_cartesian_controller
+{
+
+JointToCartesianController::JointToCartesianController()
+{
+}
+
+bool JointToCartesianController::init(hardware_interface::PositionJointInterface* hw, ros::NodeHandle& nh)
+{
+  return true;
+}
+
+void JointToCartesianController::starting(const ros::Time& time)
+{
+}
+
+void JointToCartesianController::stopping(const ros::Time& time)
+{
+}
+
+void JointToCartesianController::update(const ros::Time& time, const ros::Duration& period)
+{
+}
+
+}
