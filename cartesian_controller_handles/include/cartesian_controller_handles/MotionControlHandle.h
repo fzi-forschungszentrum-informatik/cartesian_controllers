5c8b50e3e039be7c41f3c6e40d68201e89013051
Stefan Scherzinger <scherzin@fzi.de>
2017-11-07 15:02:35 +0100

    Controller Handles: Continue implementing basic functionality

--- cartesian_controller_handles/include/cartesian_controller_handles/MotionControlHandle.h
+++ cartesian_controller_handles/include/cartesian_controller_handles/MotionControlHandle.h
@@ -13,9 +13,26 @@
 #ifndef MOTION_CONTROL_HANDLE_H_INCLUDED
 #define MOTION_CONTROL_HANDLE_H_INCLUDED
 
+// ROS
+#include <interactive_markers/interactive_marker_server.h>
+
+// TF
+#include <tf/transform_broadcaster.h>
+
+// Other
+#include <boost/shared_ptr.hpp>
+
 namespace cartesian_controller_handles
 {
 
+/**
+ * @brief Implements a drag-and-drop control handle in RViz
+ *
+ * This motion control handle can be used to control the end-effector of a
+ * robotic manipulator in Cartesian space with an interactive marker.  The
+ * motion of the marker (both orientation and translation) is broadcasted as a
+ * \a target_frame to TF, which can be followed by motion-based Cartesian controllers.
+ */
 class MotionControlHandle
 {
   public:
@@ -23,6 +40,37 @@ class MotionControlHandle
     ~MotionControlHandle();
 
   private:
+    bool init();
+
+    /**
+     * @brief Move visual marker in RViz according to user interaction
+     *
+     * This function also broadcasts a TF to where the interactive marker is
+     * supposed to be. This TF can be used as \a target_frame for \a Cartesian
+     * \a Controllers.
+     *
+     * @param feedback The message containing the current pose of the marker
+     */
+    void updateMotionControlCallback(const visualization_msgs::InteractiveMarkerFeedbackConstPtr& feedback);
+
+    /**
+     * @brief React to changes in the interactive marker menu
+     *
+     * @param feedback The message containing the current menu configuration
+     */
+    void updateMarkerMenuCallback(const visualization_msgs::InteractiveMarkerFeedbackConstPtr& feedback);
+
+    // Kinematics
+    std::string               m_robot_base_link;
+    std::string               m_end_effector_link;
+    tf::TransformBroadcaster  m_tf_broadcaster;
+
+    // Interactive marker
+    boost::shared_ptr<
+      interactive_markers::InteractiveMarkerServer> m_server;
+
+    visualization_msgs::InteractiveMarker           m_marker; //!< Controller handle for RViz
+
 };
 
 } // cartesian_controller_handles
--- cartesian_controller_handles/src/MotionControlHandle.cpp
+++ cartesian_controller_handles/src/MotionControlHandle.cpp
@@ -14,13 +14,100 @@
 
 namespace cartesian_controller_handles
 {
-  MotionControlHandle::MotionControlHandle()
-  {
-  }
 
-  MotionControlHandle::~MotionControlHandle()
+MotionControlHandle::MotionControlHandle()
+{
+  if (!init())
   {
+    throw std::logic_error("Failed to initialize motion control handle.");
   }
+}
 
+MotionControlHandle::~MotionControlHandle()
+{
+}
+
+bool MotionControlHandle::init()
+{
+  m_server.reset(new interactive_markers::InteractiveMarkerServer("test_marker","",false));
+
+  // Configure the interactive marker for usage in RViz
+
+  m_marker.header.frame_id = "base_link";
+  m_marker.header.stamp = ros::Time(0);   // makes frame_id const
+  m_marker.name = "my_marker";
+  geometry_msgs::Pose initial_pose;
+  m_marker.pose = initial_pose;
+  m_marker.description = "6D Cartesian control of the <end_effector_name> end-effector.";
+
+  // Create a sphere as a handle
+  visualization_msgs::Marker visual;
+  visual.type = visualization_msgs::Marker::SPHERE;
+  visual.scale.x = 0.01;  // bounding box in meter
+  visual.scale.y = 0.01;
+  visual.scale.z = 0.01;
+  visual.color.r = 0.6;
+  visual.color.g = 0.0;
+  visual.color.b = 0.2;
+  visual.color.a = 1.0;
+
+  // Create a non-interactive control for the appearance
+  visualization_msgs::InteractiveMarkerControl visual_control;
+  visual_control.always_visible = true;
+  visual_control.markers.push_back(visual);
+  m_marker.controls.push_back(visual_control);
+
+  // Create a control which will move the handle
+  visualization_msgs::InteractiveMarkerControl cartesian_control;
+  cartesian_control.name = "move 6D";
+  cartesian_control.interaction_mode =
+    visualization_msgs::InteractiveMarkerControl::MOVE_ROTATE_3D;
+  m_marker.controls.push_back(cartesian_control);
+
+  // Add the interactive marker to the server
+  m_server->insert(m_marker);
+
+  // Add callback for motion in RViz
+  m_server->setCallback(
+      m_marker.name,
+      boost::bind(&MotionControlHandle::updateMotionControlCallback,this,_1),
+      visualization_msgs::InteractiveMarkerFeedback::POSE_UPDATE);
+
+  // Add callback for menu interaction in RViz
+  m_server->setCallback(
+      m_marker.name,
+      boost::bind(&MotionControlHandle::updateMarkerMenuCallback,this,_1),
+      visualization_msgs::InteractiveMarkerFeedback::MENU_SELECT);
+
+  // Activate configuration
+  m_server->applyChanges();
+
+  return true;
+}
+
+void MotionControlHandle::updateMotionControlCallback(
+    const visualization_msgs::InteractiveMarkerFeedbackConstPtr& feedback)
+{
+  // Move marker in RViz
+  m_server->setPose(feedback->marker_name,feedback->pose);
+  m_server->applyChanges();
+
+  // Publish pose to TF
+  tf::Transform pose;
+  tf::poseMsgToTF(feedback->pose,pose);
+
+  m_tf_broadcaster.sendTransform(
+      tf::StampedTransform(
+        pose,
+        ros::Time::now(),
+        m_robot_base_link,
+        m_end_effector_link));
+}
+
+
+void MotionControlHandle::updateMarkerMenuCallback(
+    const visualization_msgs::InteractiveMarkerFeedbackConstPtr& feedback)
+{
+}
 
 }
