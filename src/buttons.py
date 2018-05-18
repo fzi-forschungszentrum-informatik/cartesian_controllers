a4172d808c17ebf3179e510b6de0542cca54522d
Stefan Scherzinger <scherzin@fzi.de>
2018-05-18 18:45:14 +0200

    Avoid pressing the same buttons twice in a row
    This is somewhat specific, but makes sense for switching something
    on and off, which is the primary usecase for the two spacenav buttons.

--- src/buttons.py
+++ src/buttons.py
@@ -17,12 +17,18 @@ class buttons:
         self.sub = rospy.Subscriber(self.joystick_topic, Joy, self.event_callback)
 
         self.button_cmds = rospy.get_param('button_cmds')
+        self.last_button_cmds = None
 
 
     def event_callback(self,data):
+        if data.buttons == self.last_button_cmds:
+            return
         for idx, val in enumerate(data.buttons):
             if val == 1:
+                # Prevent pressing the same buttons in a row
                 call(self.button_cmds[idx],shell=True)
+                self.last_button_cmds = data.buttons
+                return
 
 
 if __name__ == '__main__':
