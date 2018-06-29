d6e382affc1f7cb8246f467f2a934695d8ce5498
Stefan Scherzinger <scherzin@fzi.de>
2018-06-29 16:04:34 +0200

    converter: Allow an independent publishing rate for the wrenches

--- src/converter.py
+++ src/converter.py
@@ -14,27 +14,34 @@ class converter:
         self.twist_topic = rospy.get_param('~twist_topic',default="my_twist")
         self.wrench_topic = rospy.get_param('~wrench_topic',default="my_wrench")
         self.frame_id = rospy.get_param('~frame_id',default="world")
+        self.rate = rospy.Rate(rospy.get_param('~publishing_rate',default=100))
+
+        self.buffer = WrenchStamped()
 
         self.pub = rospy.Publisher(self.wrench_topic, WrenchStamped, queue_size=3)
-        self.sub = rospy.Subscriber(self.twist_topic, Twist, self.republish)
+        self.sub = rospy.Subscriber(self.twist_topic, Twist, self.twist_cb)
 
 
-    def republish(self,data):
-        wrench = WrenchStamped()
-        wrench.header.stamp     = rospy.Time.now()
-        wrench.header.frame_id  = self.frame_id
-        wrench.wrench.force.x   = data.linear.x
-        wrench.wrench.force.y   = data.linear.y
-        wrench.wrench.force.z   = data.linear.z
-        wrench.wrench.torque.x  = data.angular.x
-        wrench.wrench.torque.y  = data.angular.y
-        wrench.wrench.torque.z  = data.angular.z
-        self.pub.publish(wrench)
+    def twist_cb(self,data):
+        self.buffer.header.stamp     = rospy.Time.now()
+        self.buffer.header.frame_id  = self.frame_id
+        self.buffer.wrench.force.x   = data.linear.x
+        self.buffer.wrench.force.y   = data.linear.y
+        self.buffer.wrench.force.z   = data.linear.z
+        self.buffer.wrench.torque.x  = data.angular.x
+        self.buffer.wrench.torque.y  = data.angular.y
+        self.buffer.wrench.torque.z  = data.angular.z
+
+
+    def publish(self):
+        self.pub.publish(self.buffer)
 
 
 if __name__ == '__main__':
     conv = converter()
     try:
-        rospy.spin()
+        while not rospy.is_shutdown():
+            conv.publish()
+            conv.rate.sleep()
     except rospy.ROSInterruptException:
         pass
