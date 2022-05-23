ab7713c95c27c3da6b4a63980c7c59eb9ff52da4
Stefan Scherzinger <scherzin@fzi.de>
2022-05-23 11:50:02 +0200

    Port the *cartesian_motion_controller* to ROS2
    This is analog to the *cartesian_force_controller*.
    The distinction between position and velocity actuation via templates is
    no longer necessary. The base class takes care of sending control
    commands to all supported interfaces.
    
    We also drop the explicit mentioning of the `target_frame_topic`.
    That's conveniently solved with remappings.

--- cartesian_motion_controller/src/cartesian_motion_controller.cpp
+++ cartesian_motion_controller/src/cartesian_motion_controller.cpp
@@ -37,29 +37,189 @@
  */
 //-----------------------------------------------------------------------------
 
-// Pluginlib
-#include <pluginlib/class_list_macros.h>
-
 // Project
+#include "cartesian_controller_base/Utility.h"
+#include "controller_interface/controller_interface.hpp"
+#include "rclcpp/clock.hpp"
+#include "rclcpp/duration.hpp"
 #include <cartesian_motion_controller/cartesian_motion_controller.h>
 
-namespace position_controllers
+// Other
+#include <algorithm>
+
+namespace cartesian_motion_controller
 {
-  /**
-   * @brief Cartesian motion controller that transforms end-effector target motion into commands for a chain of position interfaces.
-   */
-  typedef cartesian_motion_controller::CartesianMotionController<
-    hardware_interface::PositionJointInterface> CartesianMotionController;
+
+CartesianMotionController::CartesianMotionController()
+: Base::CartesianControllerBase()
+{
 }
 
-namespace velocity_controllers
+#if defined CARTESIAN_CONTROLLERS_GALACTIC
+rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn CartesianMotionController::on_init()
 {
-  /**
-   * @brief Cartesian motion controller that transforms end-effector target motion into commands for a chain of velocity interfaces.
-   */
-  typedef cartesian_motion_controller::CartesianMotionController<
-    hardware_interface::VelocityJointInterface> CartesianMotionController;
+  const auto ret = Base::on_init();
+  if (ret != rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS)
+  {
+    return ret;
+  }
+
+  return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+}
+#elif defined CARTESIAN_CONTROLLERS_FOXY
+controller_interface::return_type CartesianMotionController::init(const std::string & controller_name)
+{
+  const auto ret = Base::init(controller_name);
+  if (ret != controller_interface::return_type::OK)
+  {
+    return ret;
+  }
+
+  return controller_interface::return_type::OK;
+}
+#endif
+
+rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn CartesianMotionController::on_configure(
+    const rclcpp_lifecycle::State & previous_state)
+{
+  const auto ret = Base::on_configure(previous_state);
+  if (ret != rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS)
+  {
+    return ret;
+  }
+
+  m_target_frame_subscr = get_node()->create_subscription<geometry_msgs::msg::WrenchStamped>(
+      "target_frame", 3, std::bind(&CartesianMotionController::targetFrameCallback, this, std::placeholders::_1));
+
+  return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
 }
 
-PLUGINLIB_EXPORT_CLASS(position_controllers::CartesianMotionController, controller_interface::ControllerBase)
-PLUGINLIB_EXPORT_CLASS(velocity_controllers::CartesianMotionController, controller_interface::ControllerBase)
+rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn CartesianMotionController::on_activate(
+    const rclcpp_lifecycle::State & previous_state)
+{
+  Base::on_activate(previous_state);
+
+  // Reset simulation with real joint state
+  m_current_frame = Base::m_ik_solver->getEndEffectorPose();
+
+  // Start where we are
+  m_target_frame = m_current_frame;
+  return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+}
+
+rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn CartesianMotionController::on_deactivate(
+    const rclcpp_lifecycle::State & previous_state)
+{
+  // Stop drifting by sending zero joint velocities
+  Base::computeJointControlCmds(ctrl::Vector6D::Zero(), rclcpp::Duration::from_seconds(0));
+  Base::writeJointControlCmds();
+  return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+}
+
+#if defined CARTESIAN_CONTROLLERS_GALACTIC
+controller_interface::return_type CartesianMotionController::update(const rclcpp::Time& time,
+                                                                   const rclcpp::Duration& period)
+#elif defined CARTESIAN_CONTROLLERS_FOXY
+controller_interface::return_type CartesianMotionController::update()
+#endif
+{
+  // Synchronize the internal model and the real robot
+  Base::m_ik_solver->synchronizeJointPositions(Base::m_joint_state_pos_handles);
+
+  // Forward Dynamics turns the search for the according joint motion into a
+  // control process. So, we control the internal model until we meet the
+  // Cartesian target motion. This internal control needs some simulation time
+  // steps.
+  for (int i = 0; i < Base::m_iterations; ++i)
+  {
+    // The internal 'simulation time' is deliberately independent of the outer
+    // control cycle.
+    auto internal_period = rclcpp::Duration::from_seconds(0.02);
+
+    // Compute the motion error = target - current.
+    ctrl::Vector6D error = computeMotionError();
+
+    // Turn Cartesian error into joint motion
+    Base::computeJointControlCmds(error,internal_period);
+  }
+
+  // Write final commands to the hardware interface
+  Base::writeJointControlCmds();
+
+  return controller_interface::return_type::OK;
+}
+
+ctrl::Vector6D CartesianMotionController::
+computeMotionError()
+{
+  // Compute motion error wrt robot_base_link
+  m_current_frame = Base::m_ik_solver->getEndEffectorPose();
+
+  // Transformation from target -> current corresponds to error = target - current
+  KDL::Frame error_kdl;
+  error_kdl.M = m_target_frame.M * m_current_frame.M.Inverse();
+  error_kdl.p = m_target_frame.p - m_current_frame.p;
+
+  // Use Rodrigues Vector for a compact representation of orientation errors
+  // Only for angles within [0,Pi)
+  KDL::Vector rot_axis = KDL::Vector::Zero();
+  double angle    = error_kdl.M.GetRotAngle(rot_axis);   // rot_axis is normalized
+  double distance = error_kdl.p.Normalize();
+
+  // Clamp maximal tolerated error.
+  // The remaining error will be handled in the next control cycle.
+  // Note that this is also the maximal offset that the
+  // cartesian_compliance_controller can use to build up a restoring stiffness
+  // wrench.
+  const double max_angle = 1.0;
+  const double max_distance = 1.0;
+  angle    = std::clamp(angle,-max_angle,max_angle);
+  distance = std::clamp(distance,-max_distance,max_distance);
+
+  // Scale errors to allowed magnitudes
+  rot_axis = rot_axis * angle;
+  error_kdl.p = error_kdl.p * distance;
+
+  // Reassign values
+  ctrl::Vector6D error;
+  error(0) = error_kdl.p.x();
+  error(1) = error_kdl.p.y();
+  error(2) = error_kdl.p.z();
+  error(3) = rot_axis(0);
+  error(4) = rot_axis(1);
+  error(5) = rot_axis(2);
+
+  return error;
+}
+
+void CartesianMotionController::targetFrameCallback(const geometry_msgs::msg::PoseStamped::SharedPtr target)
+{
+  if (target->header.frame_id != Base::m_robot_base_link)
+  {
+    auto& clock = *node_->get_clock();
+    RCLCPP_WARN_THROTTLE(node_->get_logger(),
+        clock, 3000,
+        "Got target pose in wrong reference frame. Expected: %s but got %s",
+        Base::m_robot_base_link.c_str(),
+        target->header.frame_id.c_str());
+    return;
+  }
+
+  m_target_frame = KDL::Frame(
+      KDL::Rotation::Quaternion(
+        target->pose.orientation.x,
+        target->pose.orientation.y,
+        target->pose.orientation.z,
+        target->pose.orientation.w),
+      KDL::Vector(
+        target->pose.position.x,
+        target->pose.position.y,
+        target->pose.position.z));
+}
+
+} // namespace
+
+// Pluginlib
+#include <pluginlib/class_list_macros.hpp>
+
+PLUGINLIB_EXPORT_CLASS(cartesian_motion_controller::CartesianMotionController, controller_interface::ControllerInterface)
