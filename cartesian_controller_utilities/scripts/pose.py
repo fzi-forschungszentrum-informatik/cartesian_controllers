713ac372fff94eae0fdede4bcca361842877d2d5
Stefan Scherzinger <scherzin@fzi.de>
2022-11-10 20:37:55 +0100

    Implement `pose.py`
    Something is still weird with the TF lookup:
    
    ```bash
    [pose.py-2] "base_link" passed to lookupTransform argument target_frame does not exist.
    ```
    and
    
    ```bash
    20:33 $ ros2 run tf2_ros tf2_echo base_link tool0
    [INFO] [1668108971.933817826] [tf2_echo]: Waiting for transform base_link ->  tool0: Invalid frame ID "base_link" passed to ca
    nTransform argument target_frame - frame does not exist
    At time 1668108972.899995998
    - Translation: [1.013, 0.260, 0.021]
    - Rotation: in Quaternion [-0.015, 0.702, 0.711, 0.034]
    ```

--- cartesian_controller_utilities/scripts/pose.py
+++ cartesian_controller_utilities/scripts/pose.py
@@ -29,9 +29,18 @@
 # POSSIBILITY OF SUCH DAMAGE.
 ################################################################################
 
+# -----------------------------------------------------------------------------
+# \file    pose.py
+#
+# \author  Stefan Scherzinger <scherzin@fzi.de>
+# \date    2022/11/09
+#
+# -----------------------------------------------------------------------------
+
+import rclpy
+from rclpy.node import Node
 import numpy as np
 import quaternion
-import rospy
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import PoseStamped
 import tf2_ros
@@ -39,7 +48,7 @@ import sys
 import time
 
 
-class converter:
+class converter(Node):
     """ Convert Twist messages to PoseStamped
 
     Use this node to integrate twist messages into a moving target pose in
@@ -48,16 +57,17 @@ class converter:
     """
 
     def __init__(self):
-        rospy.init_node('converter', anonymous=False)
+        super().__init__('converter')
 
-        self.twist_topic = rospy.get_param('~twist_topic', default="my_twist")
-        self.pose_topic = rospy.get_param('~pose_topic', default="my_pose")
-        self.frame_id = rospy.get_param('~frame_id', default="base_link")
-        self.end_effector = rospy.get_param('~end_effector', default="tool0")
-        self.rate = rospy.Rate(rospy.get_param('~publishing_rate', default=100))
+        self.twist_topic = self.declare_parameter('twist_topic', 'my_twist').value
+        self.pose_topic = self.declare_parameter('pose_topic', 'my_pose').value
+        self.frame_id = self.declare_parameter('frame_id', 'base_link').value
+        self.end_effector = self.declare_parameter('end_effector', 'tool0').value
+        period = 1.0 / self.declare_parameter('publishing_rate', 100).value
+        self.timer = self.create_timer(period, self.publish)
 
-        self.tf_buffer = tf2_ros.Buffer(cache_time=rospy.Duration(3.0))
-        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
         self.rot = np.quaternion(0, 0, 0, 1)
         self.pos = [0, 0, 0]
 
@@ -65,18 +75,19 @@ class converter:
         if not self.startup():
             sys.exit(0)
 
-        self.pub = rospy.Publisher(self.pose_topic, PoseStamped, queue_size=3)
-        self.sub = rospy.Subscriber(self.twist_topic, Twist, self.twist_cb)
+        self.pub = self.create_publisher(PoseStamped, self.pose_topic, 3)
+        self.sub = self.create_subscription(Twist, self.twist_topic, self.twist_cb, 1)
         self.last = time.time()
 
     def startup(self):
         try:
             start = self.tf_buffer.lookup_transform(
-                target_frame=self.frame_id, source_frame=self.end_effector, time=rospy.Time(0), timeout=rospy.Duration(5))
+                target_frame=self.frame_id, source_frame=self.end_effector,
+                time=rclpy.time.Time(), timeout=rclpy.time.Duration(seconds=1))
 
         except (tf2_ros.InvalidArgumentException, tf2_ros.LookupException,
                 tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
-            rospy.logwarn(e)
+            print(e)
             return False
 
         self.pos[0] = start.transform.translation.x
@@ -114,31 +125,35 @@ class converter:
         self.rot = q[-1]  # the last one is after dt passed
 
     def publish(self):
-        if not rospy.is_shutdown():
-            msg = PoseStamped()
-            msg.header.stamp = rospy.Time.now()
-            msg.header.frame_id = self.frame_id
-            msg.pose.position.x = self.pos[0]
-            msg.pose.position.y = self.pos[1]
-            msg.pose.position.z = self.pos[2]
-            msg.pose.orientation.x = self.rot.x
-            msg.pose.orientation.y = self.rot.y
-            msg.pose.orientation.z = self.rot.z
-            msg.pose.orientation.w = self.rot.w
+        msg = PoseStamped()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.header.frame_id = self.frame_id
+        msg.pose.position.x = self.pos[0]
+        msg.pose.position.y = self.pos[1]
+        msg.pose.position.z = self.pos[2]
+        msg.pose.orientation.x = self.rot.x
+        msg.pose.orientation.y = self.rot.y
+        msg.pose.orientation.z = self.rot.z
+        msg.pose.orientation.w = self.rot.w
 
-            try:
-                self.pub.publish(msg)
-            except rospy.ROSException:
-                # Swallow 'publish() to closed topic' error.
-                # This rarely happens on killing this node.
-                pass
+        try:
+            self.pub.publish(msg)
+        except:
+            # Swallow 'publish() to closed topic' error.
+            # This rarely happens on killing this node.
+            pass
 
+def main(args=None):
+    rclpy.init(args=args)
+    node = converter()
+    rclpy.spin(node)
+    rclpy.shutdown()
 
 if __name__ == '__main__':
-    conv = converter()
     try:
-        while not rospy.is_shutdown():
-            conv.publish()
-            conv.rate.sleep()
-    except rospy.ROSInterruptException:
-        pass
+        main()
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except Exception as e:
+        print(e)
+        sys.exit(1)
