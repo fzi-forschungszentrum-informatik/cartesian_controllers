5d1e032cb9ce06b4296f2b4129f936a7bff850d3
Stefan Scherzinger <scherzin@fzi.de>
2023-11-29 18:19:41 +0100

    Add a service for setting the controller's target

--- include/rackki_learning/skill_controller.h
+++ include/rackki_learning/skill_controller.h
@@ -4,7 +4,13 @@
 #include "controller_interface/controller_interface.hpp"
 #include "geometry_msgs/msg/wrench_stamped.hpp"
 #include "kdl/chain.hpp"
+#include "rackki_interfaces/srv/detail/set_target__struct.hpp"
+#include "rackki_interfaces/srv/set_target.hpp"
+#include "tf2/exceptions.h"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_listener.h"
 #include <array>
+#include <atomic>
 #include <hardware_interface/loaned_state_interface.hpp>
 #include <kdl/chainfksolvervel_recursive.hpp>
 #include <kdl/frames.hpp>
@@ -42,6 +48,7 @@ private:
   std::vector<std::pair<std::string, tensorflow::Tensor> > buildInputTensor();
   void updateJointStates();
   void cleanup();
+  bool setTarget(const std::string& target);
 
   std::thread m_serving_thread;
   tensorflow::SavedModelBundleLite m_bundle;
@@ -52,6 +59,11 @@ private:
   geometry_msgs::msg::WrenchStamped m_target_wrench;
   std::mutex m_joint_mutex;
   KDL::Frame m_target_pose;
+  std::mutex m_target_mutex;
+  std::atomic<bool> m_has_target = false;
+  rclcpp::Service<rackki_interfaces::srv::SetTarget>::SharedPtr m_set_target_server;
+  std::shared_ptr<tf2_ros::TransformListener> m_tf_listener;
+  std::unique_ptr<tf2_ros::Buffer> m_tf_buffer;
   rclcpp::Publisher<geometry_msgs::msg::WrenchStamped>::SharedPtr m_target_wrench_publisher;
 
   std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface> >
