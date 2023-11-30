6b35e269ae850c4f61251cd735f87927a1812998
Stefan Scherzinger <scherzin@fzi.de>
2023-11-30 01:36:49 +0100

    Add skeleton for serving in python

--- launch/model.launch.py
+++ launch/model.launch.py
@@ -5,21 +5,21 @@ from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
-    arg_model_path = DeclareLaunchArgument(
-        "model_path",
+    arg_model_dir = DeclareLaunchArgument(
+        "model_dir",
         default_value="/home/scherzin/src/robot_folders/checkout/rack-ki/colcon_ws/src/rackki_learning/test/models/model_1",  # noqa: E501
-        description="Absolute path to the trained model.",
+        description="Absolute path to the trained model directory.",
     )
     return LaunchDescription(
         [
-            arg_model_path,
+            arg_model_dir,
             Node(
                 package="rackki_learning",
                 executable="model.py",
                 name="model",
                 output="screen",
                 emulate_tty=True,
-                parameters=[{"model_path": LaunchConfiguration("model_path")}],
+                parameters=[{"model_dir": LaunchConfiguration("model_dir")}],
             ),
         ]
     )
--- scripts/model.py
+++ scripts/model.py
@@ -1,5 +1,60 @@
 #!/usr/bin/env python3
+import rclpy
+import tensorflow as tf
+from rclpy.node import Node
+
+
+class Server(Node):
+    def __init__(self):
+        super().__init__("server")
+        prediction_rate = 5  # Hz
+        model_dir = self.declare_parameter("model_dir", "").value
+        self.model = tf.keras.models.load_model(model_dir)
+        self.timer = self.create_timer(1.0 / prediction_rate, self.predict)
+
+    def predict(self):
+        input_data = tf.constant(
+            [
+                [
+                    [
+                        1.0,
+                        2.0,
+                        3.0,
+                        4.0,
+                        5.0,
+                        6.0,
+                        7.0,
+                        8.0,
+                        9.0,
+                        10.0,
+                        11.0,
+                        12.0,
+                        13.0,
+                        14.0,
+                        15.0,
+                        16.0,
+                        17.0,
+                        18.0,
+                        19.0,
+                    ]
+                ]
+            ],
+            dtype=tf.float32,
+        )
+        output = self.model.signatures["serving_default"](lstm_input=input_data)
+        output_tensor = output["prediction"].numpy()[0]
+        print(output_tensor)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    server = Server()
+    rclpy.spin(server)
+    rclpy.shutdown()
 
 
 if __name__ == "__main__":
-    pass
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
