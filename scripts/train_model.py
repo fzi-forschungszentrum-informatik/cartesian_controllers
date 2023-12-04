4ca9c01482e114bde31b2b0cf73efbc089882938
Stefan Scherzinger <scherzin@fzi.de>
2023-12-04 20:29:37 +0100

    Find good training settings
    This model looks very promising in simulation.

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -9,9 +9,9 @@ def main():
     evaluation_files = os.path.join(os.getcwd(), "../datasets/put/eval")
     training_data = Dataset(training_files, sequence_length=25)
     evaluation_data = Dataset(evaluation_files, sequence_length=25)
-    model = Model(n_nodes=100, n_gaussians=12)
+    model = Model(n_nodes=150, n_gaussians=4)
     model.train(
-        training_data, evaluation_data, training_iterations=10000, batch_size=256
+        training_data, evaluation_data, training_iterations=20000, batch_size=128
     )
 
     model_dir = os.path.join(os.getcwd(), "../models/put")
